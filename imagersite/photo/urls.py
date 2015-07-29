a0cc75e20ae11b41ab7edd8d28ea75aa3923cb2c
wesleywooten <wootenwesley@gmail.com>
2015-07-29 16:52:33 -0700

    fixes

--- imagersite/photo/urls.py
+++ imagersite/photo/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import library_view, album_view, photo_view
 
 urlpatterns = [
-    url(r'library/', library_view),
-    url(r'album/<album_id>\d+', album_view),
-    url(r'photos/<photo_id>\d+', photo_view)
+    url(r'library/$', library_view),
+    url(r'album/(?P<album_id>\d+)$', album_view),
+    url(r'photos/(?P<photo_id>\d+)$', photo_view)
 ]
--- imagersite/photo/views.py
+++ imagersite/photo/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
-from .models import Albums, Photos
+from .models import Album, Photos
 
 
 def library_view(request):
-    albums = Albums.objects.filter(published='public').all()
+    albums = Album.objects.filter(published='public').all()
     return render(request, 'library.html', context={'albums': albums})
 
 
 def album_view(request, album_id=0):
-    album = Albums.objects.get(id=album_id)
-    if album.published != 'public':
+    try:
+        album = Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+        return render(request, 'error.html',
+                      context={'error_type': '404 Not Found'})
         render(request, 'error.html', context={'error_type': '403 Forbiddon'})
     return render(request, 'album.html', context={'album': album})
 
 
 def photo_view(request, photo_id=0):
-    photo = Photos.objects.get(id=photo_id)
+    try:
+        photo = Photos.objects.get(id=photo_id)
+    except Photos.DoesNotExist:
+        return render(request, 'error.html',
+                      context={'error_type': '404 Not Found'})
     if photo.published != 'public':
         render(request, 'error.html', context={'error_type': '403 Forbiddon'})
     return render(request, 'photo.html', context={'photo': photo})
