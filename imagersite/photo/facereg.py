74e49df98e8c13988e97a4984d129dd015b6c149
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-07-30 17:19:58 -0700

    Create facereg.py
    in merge conflict

---
+++ imagersite/photo/facereg.py
@@ -0,0 +1,31 @@
+import Algorithmia
+import base64
+
+
+input = <somefile.jpg>
+request = urllib2.Request('https://api.algorithmia.com/v1/algo/ANaimi/FaceDetection/0.1.0')
+request.add_header('Content-Type', 'application/json')
+request.add_header('Authorization', 'API_KEY')
+response = urllib2.urlopen(request, json.dumps(input))
+print response.read()
+
+API_KEY = 'insert later'
+
+
+def get_faces(path):
+    with open(path, 'rb') as img:
+        bimage = base64.b64encode(img.read())
+
+    Algorithmia.apiKey = 'API_KEY'
+    result = Algorithmia.algo('/algo/ANaimi/FaceDetection/0.1.0').pipe(biamge)
+
+    faces = []
+    for rect in result:
+        face = Face()
+        face.name = person_name()
+        face.x = rect['x']
+        face.y = rect['y']
+        face.width = rect['width']
+        face.height = rect['height']
+        faces.append(face)
+        return faces
