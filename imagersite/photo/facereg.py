2b7e48e701e48260fdc8cd4b9cba7832e7579941
wesleywooten <wootenwesley@gmail.com>
2015-08-05 16:51:20 -0700

    updated facerecog

---
+++ imagersite/photo/facereg.py
@@ -0,0 +1,31 @@
+import Algorithmia
+import base64
+from models import Face
+
+
+def get_faces(path):
+    with open(path, 'rb') as img:
+        bimage = base64.b64encode(img.read())
+
+    Algorithmia.apiKey = 'Simple sim5DuXYJCZpgSU8Tl2NnoD5lNY1'
+    result = Algorithmia.algo('/ANaimi/FaceDetection').pipe(bimage)
+
+    faces = []
+    for rect in result:
+        face = Face()
+        face.name = "Anon"
+        face.x = rect['x']
+        face.y = rect['y']
+        face.width = rect['width']
+        face.height = rect['height']
+        faces.append(face)
+    for face in faces:
+        face.save()
+    return faces
+
+
+def set_faces(request, photo_id):
+    face_id = request.POST.get('photo_id', '0')
+    face = Face.objects.get(id=face_id)
+    face.name = Face.objects.get('name', 'Unknown')
+    face.save()
