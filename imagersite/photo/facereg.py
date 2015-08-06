8233311142c6614a0032d50b2bff93104a370c00
wesleywooten <wootenwesley@gmail.com>
2015-08-06 15:59:15 -0700

    merge conflicts and some wiring

--- imagersite/photo/facereg.py
+++ imagersite/photo/facereg.py
@@ -1,13 +1,15 @@
 import Algorithmia
 import base64
 from models import Face
+from imagersite.settings import MEDIA_ROOT
 
 
 def get_faces(path):
+    path = MEDIA_ROOT + "/" + path
     with open(path, 'rb') as img:
         bimage = base64.b64encode(img.read())
 
-    Algorithmia.apiKey = 'Simple totally_real_api_key'
+    Algorithmia.apiKey = 'Simple sim5DuXYJCZpgSU8Tl2NnoD5lNY1'  # 'Simple totally_real_api_key'
     result = Algorithmia.algo('/ANaimi/FaceDetection').pipe(bimage)
 
     faces = []
