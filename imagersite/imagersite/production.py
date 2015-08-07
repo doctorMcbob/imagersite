d4d9f12fb2eb7d8c3a65ffba4fffe1ed7f1b2209
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-07 12:24:19 -0700

    added production settings

---
+++ imagersite/imagersite/production.py
@@ -0,0 +1,8 @@
+from settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = ['ec2-52-26-163-206.us-west-2.compute.amazonaws.com',
+                 'localhost']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
